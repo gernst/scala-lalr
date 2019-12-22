d0da46b0989f6a78899b85f3c7be3332881ab06b
Gidon Ernst <gidonernst@gmail.com>
2019-12-22 17:18:24 +0100

    some refactoring

--- scalalr/src/scalalr/parser/package.scala
+++ scalalr/src/scalalr/parser/package.scala
@@ -3,14 +3,14 @@ package scalalr
 package object parser {
   import scalalr.util.Terminal
 
-  def id[A](a: A): A = a
-
-  implicit class SetOps[A](self: Set[A]) {
-    def disjoint(that: Set[A]) = {
-      (self & that).isEmpty
-    }
-  }
-
   sealed trait Error extends Exception
   case class UnexpectedSymbol(symbol: Terminal, position: Position) extends Error
+  
+  object timer {
+    import util.Timer
+    
+    object items extends Timer("items")
+    object transitions extends Timer("transitions")
+    object merge extends Timer("merge")
+  }
 }
